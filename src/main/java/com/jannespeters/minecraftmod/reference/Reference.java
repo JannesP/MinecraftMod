bc675e73de74c1643678e457af63fdfa2cb6611d
Jannes Peters <jannes.peters@arcor.de>
2014-10-25 23:29:14 +0200

    Added Reference Class and changed hardcoded strings to Reference.*

---
+++ src/main/java/com/jannespeters/minecraftmod/reference/Reference.java
@@ -0,0 +1,13 @@
+package com.jannespeters.minecraftmod.reference;
+
+/**
+ * Created by Jannes Peters on 10/25/2014.
+ */
+public class Reference {
+    public static final String MOD_ID = "MinecraftMod";
+    public static final String MOD_NAME = "Minecraft Sample Mod";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+    public static final String VERSION = "0.1";
+    public static final String SERVER_PROXY_CLASS = "com.jannespeters.minecraft.proxy.ClientProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.jannespeters.minecraft.proxy.ServerProxy";
+}
